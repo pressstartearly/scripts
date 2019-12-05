8e66256f5142c2ff08b31d0fb11b71b4a98c2478
pressstartearly <57644769+pressstartearly@users.noreply.github.com>
2019-12-05 08:56:36 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -25,6 +25,25 @@ curl -sL https://deb.nodesource.com/setup_10.x | bash -
 ## Install nodejs
 apt install -y nodejs
 
+## -----UFW-----
+
+apt-get install -y ufw
+ufw allow OpenSSH
+ufw allow 22
+ufw allow 80
+ufw allow 443
+ufw allow 25565
+ufw allow 25566
+ufw allow 25567
+ufw allow 25568
+ufw allow 25569
+ufw allow 8080
+ufw allow 2022
+ufw default deny incoming
+ufw default allow outgoing
+ufw enable
+
+
 mkdir -p /srv/daemon /srv/daemon-data
 
 cd /srv/daemon
@@ -39,7 +58,7 @@ npm install --only=production
 
 sudo add-apt-repository ppa:certbot/certbot
 sudo apt update
-sudo apt install certbot
+sudo apt install -y certbot
 
 echo "What is the domain name for this node?"
 read ccdomain
@@ -53,3 +72,26 @@ echo "@monthly certbot renew" >> mycron
 #install new cron file
 crontab mycron
 rm mycron
+
+echo "What is the auto config command?"
+read ccconfig
+$ccconfig
+
+echo "[Unit]
+Description=Pterodactyl Wings Daemon
+After=docker.service
+
+[Service]
+User=root
+#Group=some_group
+WorkingDirectory=/srv/daemon
+LimitNOFILE=4096
+PIDFile=/var/run/wings/daemon.pid
+ExecStart=/usr/bin/node /srv/daemon/src/index.js
+Restart=on-failure
+StartLimitInterval=600
+
+[Install]
+WantedBy=multi-user.target" >> /etc/systemd/system/wings.service
+
+systemctl enable --now wings
