78a98b6ea9f0a9795723cb5473f41b62b772146f
pressstartearly <57644769+pressstartearly@users.noreply.github.com>
2019-12-15 09:34:03 -0500

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,11 +1,12 @@
-datecc=$(date '+%d/%m/%Y-%H:%M:%S');
+datecc=$(date '+%d-%m-%Y-%H-%M-%S');
 backupName=backup-$HOSTNAME-$datecc.zip
 
-zip /srv/daemon-data /srv/backups/$backupName
+zip -r  /srv/backups/$backupName /srv/daemon-data/*
 
 aws s3 cp /srv/backups/$backupName s3://dedimc.backups/$backupName
 
 rm /srv/backups/$backupName
 
 echo "Done"
+
 
