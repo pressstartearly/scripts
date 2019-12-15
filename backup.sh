63af27af613c8313fcec5fe4357e677e54351987
pressstartearly <57644769+pressstartearly@users.noreply.github.com>
2019-12-15 09:04:15 -0500

    Beta1

--- backup.sh
+++ backup.sh
@@ -1,1 +1,10 @@
-zip /srv/daemon-data backup-$HOSTNAME-date +"%Y-%m-%d-%T".zip
+backupName = backup-$HOSTNAME-date +"%Y-%m-%d-%T".zip
+
+zip /srv/daemon-data /srv/backups/$backupName
+
+aws s3 cp /srv/backups/$backupName s3://dedimc.backups/$backupName
+
+rm /srv/backups/$backupName
+
+echo "Done"
+
