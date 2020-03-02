2d620817f9cf356297dc2d5364b7f755253e92c6
Caleb <57644769+pressstartearly@users.noreply.github.com>
2020-03-01 23:47:38 -0500

    Create cpufreq.sh

---
+++ cpufreq.sh
@@ -0,0 +1,9 @@
+apt-get install -y cpufrequtils
+
+echo "# cpu frequency scaling
+ENABLE=\"true\"
+GOVERNOR=\"performance\"
+MAX_SPEED=\"0\"
+MIN_SPEED=\"0\" " > /etc/default/cpufrequtils
+
+systemctl restart cpufrequtils
