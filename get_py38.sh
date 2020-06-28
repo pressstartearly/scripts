9963ba0073434604b5165f8998da3c2dde2b96ec
Caleb <57644769+pressstartearly@users.noreply.github.com>
2020-06-28 12:24:31 -0400

    Create get_py38.sh

---
+++ get_py38.sh
@@ -0,0 +1,8 @@
+sudo apt update
+sudo apt install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libsqlite3-dev libreadline-dev libffi-dev curl libbz2-dev
+cd /opt
+sudo wget https://www.python.org/ftp/python/3.8.3/Python-3.8.3.tgz
+sudo tar xzf Python-3.8.3.tgz
+cd Python-3.8.3
+sudo ./configure --enable-optimizations
+sudo make altinstall
