bfe19d672bac705848147cd58f9ac41c6d59eaea
Caleb <57644769+pressstartearly@users.noreply.github.com>
2020-07-03 14:29:28 -0400

    Added Partial Influx

--- do_grafana.sh
+++ do_grafana.sh
@@ -123,4 +123,38 @@ systemctl daemon-reload
 systemctl enable prometheus
 systemctl start prometheus
 
+wget -qO- https://repos.influxdata.com/influxdb.key | sudo apt-key add -
+source /etc/os-release
+echo "deb https://repos.influxdata.com/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/influxdb.list
+
+sudo apt-get update && sudo apt-get install influxdb
+sudo systemctl unmask influxdb.service
+sudo systemctl start influxdb
+
+# Before adding Influx repository, run this so that apt will be able to read the repository.
+
+sudo apt-get update && sudo apt-get install apt-transport-https
+
+# Add the InfluxData key
+
+wget -qO- https://repos.influxdata.com/influxdb.key | sudo apt-key add -
+source /etc/os-release
+test $VERSION_ID = "7" && echo "deb https://repos.influxdata.com/debian wheezy stable" | sudo tee /etc/apt/sources.list.d/influxdb.list
+test $VERSION_ID = "8" && echo "deb https://repos.influxdata.com/debian jessie stable" | sudo tee /etc/apt/sources.list.d/influxdb.list
+test $VERSION_ID = "9" && echo "deb https://repos.influxdata.com/debian stretch stable" | sudo tee /etc/apt/sources.list.d/influxdb.list
+test $VERSION_ID = "10" && echo "deb https://repos.influxdata.com/debian buster stable" | sudo tee /etc/apt/sources.list.d/influxdb.list
+
+sudo apt-get update && sudo apt-get install telegraf
+sudo systemctl start telegraf
+
+cd /opt && git clone https://github.com/ratibor78/srvstatus.git
+cd /opt/srvstatus
+virtualenv venv && source venv/bin/activate
+pip install -r requirements.txt
+chmod +x ./service.py
+
+
+
+
+
 echo "Done! Make sure that both apps are running properly and not giving any errors"
